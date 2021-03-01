a1899e8c253b63f57bdc5d47dd75ac02e75bd391
German <bz5h7@yandex.ru>
2021-03-01 17:04:40 +0300

    feat: implement function to check if given bracket sequence is valid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0 ) {
+    return false;
+  }
+  const configArray = bracketsConfig.map(brackets => brackets.join(''));
+  const bracketsArray = str.split('');
+  const stack = [];
+
+  for (let i = 0; i < bracketsArray.length; i++) {
+    const current = bracketsArray[i];
+    stack.push(current);
+    if (stack.length > 1) {
+      const pair = [stack[stack.length - 2], stack[stack.length - 1]].join('')
+      if (configArray.some(e => e === pair)) {
+        stack.splice(-2, 2);
+      }
+    }
+  }
+  return stack.length === 0;
 }
